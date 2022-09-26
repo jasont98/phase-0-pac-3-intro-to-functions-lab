a8c0af87f71e801bd8338ce7f1c4c6e8ddcdd868
Hoang Tran <jashoang2583@gmail.com>
2022-09-26 14:58:29 -0700

    commit tree 530455fb250480b4ec600f3339228a35db433584

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+    "Hello!".toUpperCase();
+    return string.toUpperCase();
+}
+function whisper(string) {
+    "Hello!".toLowerCase();
+    return string.toLowerCase();
+}
+function logShout(string) {
+    "Hello!".toUpperCase();
+    console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+    "Hello!".toLowerCase();
+    console.log(string.toLowerCase());
+}
+const string = "Let\s have dinner together!".toUpperCase;
+function sayHiToHeadphonedRoomate(string) {
+    "I can\t hear you!".toUpperCase
+    return string.toUpperCase;
+}
+
